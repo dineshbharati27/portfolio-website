c00fdff30ddb859a97ab9269696821b125e87e66
dinesh bharati <dineshbharati705@gmail.com>
2024-11-06 10:50:07 +0530

    first commit

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import "../Components/Global.css";
+
+const handlecolorName = () => {
+  let naam = document.getElementById("naam");
+  let borderColorName = document.getElementById("borderColorName");
+  naam.style.color = "#FE7878";
+  borderColorName.style.border = "1px solid #FE7878";
+};
+
+const handlecolorEmail = () => {
+  let email = document.getElementById("email");
+  let borderColorEmail = document.getElementById("borderColorEmail");
+  email.style.color = "#1B74E4";
+  borderColorEmail.style.border = "1px solid #1B74E4";
+};
+
+const handlecolorMessage = () => {
+  let message = document.getElementById("message");
+  let borderColorMessage = document.getElementById("borderColorMessage");
+  message.style.color = "#CE65F3";
+  borderColorMessage.style.border = "1px solid #CE65F3";
+};
+
+const Contact = ({ bgcolor, darkmode }) => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", "6933971e-34ea-4fd5-bba7-86bc77ccfed9");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div
+      className={`${
+        darkmode === "dark" ? `bg-${bgcolor}` : `bg-white`
+      } p-10 rounded-lg flex flex-col gap-10`}
+    >
+      <h1
+        className="text-3xl font-bold"
+        style={{ color: `${bgcolor === "white" ? "#000000" : "white"}` }}
+      >
+        Contact
+      </h1>
+
+      <div
+        className="flex flex-col lg:w-11/12 sm:w-full gap-2 p-5 rounded-lg"
+        style={{
+          background: `${bgcolor === "white" ? "#f3f6f6" : "#1D1D1D"}`,
+        }}
+      >
+        <div className="my-3">
+          <h1
+            className="text-2xl"
+            style={{
+              color: `${bgcolor === "white" ? "#000000" : "#A6A6A6"}`,
+            }}
+          >
+            Have a project or idea in mind?
+          </h1>{" "}
+          <h1
+            className="text-2xl font-bold"
+            style={{
+              color: `${bgcolor === "white" ? "#000000" : "white"}`,
+            }}
+          >
+            Let's collaborate and make it a reality!
+          </h1>
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <div className="flex flex-col">
+            <label
+              id="naam"
+              style={{
+                color: `${bgcolor === "white" ? "#000000" : "#A6A6A6"}`,
+              }}
+            >
+              Name*
+            </label>
+
+            <input
+              type="text"
+              className="w-10/12 p-2 border border-gray-300 outline-none rounded"
+              style={{
+                background: `${bgcolor === "white" ? "#f3f6f6" : "#1D1D1D"}`,
+                color: `${bgcolor === "white" ? "#000000" : "white"}`,
+              }}
+              onClick={handlecolorName}
+              name="name"
+              id="borderColorName"
+              required
+            />
+          </div>
+
+          <div className="my-10 flex flex-col">
+            <label
+              id="email"
+              style={{
+                color: `${bgcolor === "white" ? "#000000" : "#A6A6A6"}`,
+              }}
+            >
+              Email*
+            </label>
+
+            <input
+              type="text"
+              className="w-10/12 p-2 border border-gray-300 outline-none rounded"
+              style={{
+                background: `${bgcolor === "white" ? "#f3f6f6" : "#1D1D1D"}`,
+                color: `${bgcolor === "white" ? "#000000" : "white"}`,
+              }}
+              onClick={handlecolorEmail}
+              name="email"
+              id="borderColorEmail"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label
+              id="message"
+              style={{
+                color: `${bgcolor === "white" ? "#000000" : "#A6A6A6"}`,
+              }}
+            >
+              Message*
+            </label>
+            <textarea
+              className="w-10/12 p-2 border border-gray-300 outline-none rounded"
+              style={{
+                background: `${bgcolor === "white" ? "#f3f6f6" : "#1D1D1D"}`,
+                color: `${bgcolor === "white" ? "#000000" : "white"}`,
+              }}
+              onClick={handlecolorMessage}
+              name="message"
+              id="borderColorMessage"
+              rows="4"
+              required
+            />
+          </div>
+
+          <button
+            className="my-2 w-32 h-11 hover:bg-blue-500 hover:text-white font-bold rounded-lg btn-submit border border-blue-500"
+            style={{
+              background: `${bgcolor === "white" ? "#f3f6f6" : "#1D1D1D"}`,
+              color: `${bgcolor === "white" ? "#000000" : "white"}`,
+            }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/Components/SocialLinks/assets.js
@@ -0,0 +1,130 @@
+
+import ecommerce_project from './ecommerce_project.png'
+import portfolio_project from './portfolio_project.png'
+import text_project from './text_project.png'
+import resume_builder_project from './resume_builder_project.png'
+
+import myPhoto from './MyPhoto.png'
+import resume from './Resume.pdf'
+import resume_png from './resume_png.png'
+
+import html from "./html.png"
+import css from "./css.png"
+import js from "./js.png"
+import react from "./react.png"
+import nodejs from "./nodejs.png"
+import expressJs from "./expressjs.png"
+import mongodb from "./mongodb.png"
+import postman from "./postman.png"
+import git from "./git.png"
+import tailwind from "./tailwind.png"
+import github from "./github.png"
+import bootstrap from "./bootstrap.png"
+import c from './c.png'
+import java from './java.png'
+import redux from './redux.png'
+import netlify from './netlify.png'
+import dsa from './dsa.png'
+
+
+export const assets ={
+    myPhoto,
+    resume,
+    resume_png,
+    ecommerce_project,
+    portfolio_project,
+    text_project,
+    resume_builder_project
+}
+
+
+export const skills =
+  [
+    {
+      skill:"Js",
+      png:js,  
+    },
+    {
+      skill:"C",
+      png:c,
+      
+    },
+    {
+      skill:"Java",
+      png:java,
+      
+    },
+    {
+      skill:"HTML",
+      png:html,
+      
+    },
+    {
+      skill:"Css",
+      png:css,
+      
+    },
+    {
+      skill:"ReactJs",
+      png:react,
+      
+    },
+    {
+      skill:"redux",
+      png:redux,
+      
+    },
+    {
+      skill:"Tailwind",
+      png:tailwind,
+      
+    },
+    {
+      skill:"Bootstrap",
+      png:bootstrap,
+      
+    },
+    // {
+    //   skill:"Nodejs",
+    //   png:nodejs,
+      
+    // },
+    // {
+    //   skill:"ExpressJs",
+    //   png:expressJs,
+      
+    // },
+    // {
+    //   skill:"MongodDb",
+    //   png:mongodb,
+      
+    // },
+  ]
+
+export const otherSkills=
+  [
+   {
+    skill:"DSA",
+    png:dsa
+   },
+   {
+    skill:"Git",
+    png:git
+   },
+
+   {
+    skill:"Github",
+    png:github
+   },
+   {
+    skill:"Netlify",
+    png:netlify
+   }
+   
+  //  {
+  //   skill:"Postman",
+  //   png:postman
+  //  },
+  ]
+
+
